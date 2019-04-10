481a267a24eb4c27dd0844cd9b8c3bbf0212bef6
baiyan <A0980381643@gmail.com>
2019-04-10 18:50:49 +0800

    fix index banner

--- js/all.js
+++ js/all.js
@@ -1,28 +1,37 @@
 $(document).ready(function () {
 
-  $("#bannerBg-1").hover(function () {
+  $("#banner-1").hover(function () {
 
-    $("#bannerImg-1").css("filter", "blur(0)");
-  }, function () {
-    $("#bannerImg-1").css("filter", "blur(5px)");
+      $("#bannerImg-1").css("filter", "blur(0)");
+      $("#bannerBg-1").css("background-color", "rgba(255, 225, 128, 0.8)");
+    }, function () {
+      $("#bannerImg-1").css("filter", "blur(5px)");
+      $("#bannerBg-1").css("background-color", "rgba(63, 93, 69, 0.8)");
+    },
 
-  });
+  );
 
-  $("#bannerBg-2").hover(function () {
+  $("#banner-2").hover(function () {
 
-    $("#bannerImg-2").css("filter", "blur(0)");
-  }, function () {
-    $("#bannerImg-2").css("filter", "blur(5px)");
+      $("#bannerImg-2").css("filter", "blur(0)");
+      $("#bannerBg-2").css("background-color", "rgba(255, 225, 128, 0.8)");
+    }, function () {
+      $("#bannerImg-2").css("filter", "blur(5px)");
+      $("#bannerBg-2").css("background-color", "rgba(63, 93, 69, 0.8)");
+    }
 
-  });
+  );
 
-  $("#bannerBg-3").hover(function () {
+  $("#banner-3").hover(function () {
 
-    $("#bannerImg-3").css("filter", "blur(0)");
-  }, function () {
-    $("#bannerImg-3").css("filter", "blur(5px)");
+      $("#bannerImg-3").css("filter", "blur(0)");
+      $("#bannerBg-3").css("background-color", "rgba(255, 225, 128, 0.8)");
+    }, function () {
+      $("#bannerImg-3").css("filter", "blur(5px)");
+      $("#bannerBg-3").css("background-color", "rgba(63, 93, 69, 0.8)");
+    }
 
-  });
+  );
 
 
 });
